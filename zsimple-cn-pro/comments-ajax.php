c64ee8021423c3edccc5931f570030e60ba65991
zwwooooo <zwwooooo@gmail.com>
2017-01-06 22:29:48 +0800

    v1.1.0b1: 更新 comments-ajax.php 到最新版本的 wp-comments-post.php

--- zsimple-cn-pro/comments-ajax.php
+++ zsimple-cn-pro/comments-ajax.php
@@ -6,104 +6,61 @@
  */
 
 if ( 'POST' != $_SERVER['REQUEST_METHOD'] ) {
+	$protocol = $_SERVER['SERVER_PROTOCOL'];
+	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0' ) ) ) {
+		$protocol = 'HTTP/1.0';
+	}
+
 	header('Allow: POST');
-	header('HTTP/1.1 405 Method Not Allowed');
+	header("$protocol 405 Method Not Allowed");
 	header('Content-Type: text/plain');
 	exit;
 }
 
 /** Sets up the WordPress Environment. */
+#require( dirname(__FILE__) . '/wp-load.php' );
 require( realpath(dirname(dirname(dirname(dirname($_SERVER["SCRIPT_FILENAME"]))))).'/wp-load.php' );
 
 nocache_headers();
 
-$comment_post_ID = isset($_POST['comment_post_ID']) ? (int) $_POST['comment_post_ID'] : 0;
-
-$post = get_post($comment_post_ID);
-
-if ( empty($post->comment_status) ) {
-	do_action('comment_id_not_found', $comment_post_ID);
-	exit;
-}
-
-// get_post_status() will get the parent status for attachments.
-$status = get_post_status($post);
-
-$status_obj = get_post_status_object($status);
-
- if ( !comments_open($comment_post_ID) ) {
-	do_action('comment_closed', $comment_post_ID);
-	wp_die( __('Sorry, comments are closed for this item.') );
-} elseif ( 'trash' == $status ) {
-	do_action('comment_on_trash', $comment_post_ID);
-	wp_die(__('Invalid comment status.')); // 將 exit 改為錯誤提示
-} elseif ( !$status_obj->public && !$status_obj->private ) {
-	do_action('comment_on_draft', $comment_post_ID);
-	wp_die(__('Invalid comment status.')); // 將 exit 改為錯誤提示
-} elseif ( post_password_required($comment_post_ID) ) {
-	do_action('comment_on_password_protected', $comment_post_ID);
-	wp_die(__('Password Protected')); // 將 exit 改為錯誤提示
-} else { 
-	do_action('pre_comment_on_post', $comment_post_ID);
-}
-
-$comment_author       = ( isset($_POST['author']) )  ? trim(strip_tags($_POST['author'])) : null;
-$comment_author_email = ( isset($_POST['email']) )   ? trim($_POST['email']) : null;
-$comment_author_url   = ( isset($_POST['url']) )     ? trim($_POST['url']) : null;
-$comment_content      = ( isset($_POST['comment']) ) ? trim($_POST['comment']) : null;
-
-// If the user is logged in
-$user = wp_get_current_user();
-if ( $user->exists() ) {
-	if ( empty( $user->display_name ) )
-		$user->display_name=$user->user_login;
-	$comment_author       = $wpdb->escape($user->display_name);
-	$comment_author_email = $wpdb->escape($user->user_email);
-	$comment_author_url   = $wpdb->escape($user->user_url);
-	if ( current_user_can('unfiltered_html') ) {
-		if ( wp_create_nonce('unfiltered-html-comment_' . $comment_post_ID) != $_POST['_wp_unfiltered_html_comment'] ) {
-			kses_remove_filters(); // start with a clean slate
-			kses_init_filters(); // set up the filters
-		}
+$comment = wp_handle_comment_submission( wp_unslash( $_POST ) );
+if ( is_wp_error( $comment ) ) {
+	$data = intval( $comment->get_error_data() );
+	if ( ! empty( $data ) ) {
+		#wp_die( '<p>' . $comment->get_error_message() . '</p>', __( 'Comment Submission Failure' ), array( 'response' => $data, 'back_link' => true ) );
+		wp_die( __($comment->get_error_message()) );
+	} else {
+		exit;
 	}
-} else {
-	if ( get_option('comment_registration') || 'private' == $status )
-		wp_die( __('Sorry, you must be logged in to post a comment.') );
 }
 
-$comment_type = '';
+$user = wp_get_current_user();
 
-if ( get_option('require_name_email') && !$user->exists() ) {
-	if ( 6 > strlen($comment_author_email) || '' == $comment_author )
-		wp_die( __('<strong>ERROR</strong>: please fill the required fields (name, email).') );
-	elseif ( !is_email($comment_author_email))
-		wp_die( __('<strong>ERROR</strong>: please enter a valid email address.') );
-}
+/**
+ * Perform other actions when comment cookies are set.
+ *
+ * @since 3.4.0
+ *
+ * @param WP_Comment $comment Comment object.
+ * @param WP_User    $user    User object. The user may not exist.
+ */
+do_action( 'set_comment_cookies', $comment, $user );
 
-if ( '' == $comment_content )
-	wp_die( __('<strong>ERROR</strong>: please type a comment.') );
+#$location = empty( $_POST['redirect_to'] ) ? get_comment_link( $comment ) : $_POST['redirect_to'] . '#comment-' . $comment->comment_ID;
 
-$comment_parent = isset($_POST['comment_parent']) ? absint($_POST['comment_parent']) : 0;
+/**
+ * Filters the location URI to send the commenter after posting.
+ *
+ * @since 2.0.5
+ *
+ * @param string     $location The 'redirect_to' URI sent via $_POST.
+ * @param WP_Comment $comment  Comment object.
+ */
+#$location = apply_filters( 'comment_post_redirect', $location, $comment );
 
-$commentdata = compact('comment_post_ID', 'comment_author', 'comment_author_email', 'comment_author_url', 'comment_content', 'comment_type', 'comment_parent', 'user_ID');
+#wp_safe_redirect( $location );
+#exit;
 
-$comment_id = wp_new_comment( $commentdata );
-
-$comment = get_comment($comment_id);
-do_action('set_comment_cookies', $comment, $user);
-
-// $location = empty($_POST['redirect_to']) ? get_comment_link($comment_id) : $_POST['redirect_to'] . '#comment-' . $comment_id; //取消原有的刷新重定向
-// $location = apply_filters('comment_post_redirect', $location, $comment);
-
-// wp_safe_redirect( $location );
-// exit;
-
-$comment_depth = 1;   //为评论的 class 属性准备的 by willin
-$tmp_c = $comment;
-while($tmp_c->comment_parent != 0){
-$comment_depth++;
-$tmp_c = get_comment($tmp_c->comment_parent);
-}
 //以下是评论样式，根据你的模板样式copy覆盖，移除“reply”部分
 ?>
 <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
