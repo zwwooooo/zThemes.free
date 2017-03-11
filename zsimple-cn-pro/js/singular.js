f1c7552801d3c5d29f9ba275ed353074821046fb
zwwooooo <zwwooooo@gmail.com>
2017-03-12 00:57:38 +0800

    v1.1.1 release. - 修正 singular.js 格式错误

--- zsimple-cn-pro/js/singular.js
+++ zsimple-cn-pro/js/singular.js
@@ -136,43 +136,43 @@
 				return arrayPageSize;
 			};
 		};
+	};
 
-		//// 存档页面 jQ伸缩
-		zdo_modules_S.Archives = function() {
-			var $a = $('#archives'),
-				$m = $('.al_mon', $a),
-				$l = $('.al_post_list', $a),
-				$l_f = $('.al_post_list:first', $a);
-			$l.hide();
-			$l_f.show();
-			$m.css('cursor', 's-resize').on('click', function(){
-				$(this).next().slideToggle(400);
-			});
-			var animate = function(index, status, s) {
-				if (index > $l.length) {
-					return;
-				}
-				if (status == 'up') {
-					$l.eq(index).slideUp(s, function() {
-						animate(index+1, status, (s-10<1)?0:s-10);
-					});
-				} else {
-					$l.eq(index).slideDown(s, function() {
-						animate(index+1, status, (s-10<1)?0:s-10);
-					});
-				}
-			};
-			$('#al_expand_collapse').on('click', function(e){
-				e.preventDefault();
-				if ( $(this).data('s') ) {
-					$(this).data('s', '');
-					animate(0, 'up', 100);
-				} else {
-					$(this).data('s', 1);
-					animate(0, 'down', 100);
-				}
-			});
+	//// 存档页面 jQ伸缩
+	zdo_modules_S.Archives = function() {
+		var $a = $('#archives'),
+			$m = $('.al_mon', $a),
+			$l = $('.al_post_list', $a),
+			$l_f = $('.al_post_list:first', $a);
+		$l.hide();
+		$l_f.show();
+		$m.css('cursor', 's-resize').on('click', function(){
+			$(this).next().slideToggle(400);
+		});
+		var animate = function(index, status, s) {
+			if (index > $l.length) {
+				return;
+			}
+			if (status == 'up') {
+				$l.eq(index).slideUp(s, function() {
+					animate(index+1, status, (s-10<1)?0:s-10);
+				});
+			} else {
+				$l.eq(index).slideDown(s, function() {
+					animate(index+1, status, (s-10<1)?0:s-10);
+				});
+			}
 		};
+		$('#al_expand_collapse').on('click', function(e){
+			e.preventDefault();
+			if ( $(this).data('s') ) {
+				$(this).data('s', '');
+				animate(0, 'up', 100);
+			} else {
+				$(this).data('s', 1);
+				animate(0, 'down', 100);
+			}
+		});
 	};
 
 	//// 评论 ajax 提交
